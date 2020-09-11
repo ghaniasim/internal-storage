67c30dc6a6e3fdc8c6ebc6cf0aad909228a41457
ghani-asim <asim.ghani@metropolia.fi>
2020-09-11 18:20:31 +0300

    add screenshots

--- app/src/main/java/com/example/internalstorage/SecondFragment.kt
+++ app/src/main/java/com/example/internalstorage/SecondFragment.kt
@@ -1,7 +1,6 @@
 package com.example.internalstorage
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_second.*
